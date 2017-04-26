7a718f9b6dcea965321c0e6ce6f96e14366efe36
sunly9064 <lw5853103@163.com>
2017-04-27 00:26:36 +0800

    add dgtle nav

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="nav-box">
+      <div class="nav ">
+        <div class="l">
+          <h1><a href="javascript:;"><img src="./assets/images/portal_b.png" alt=""></a></h1>
+          <el-menu theme="" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">我的工作台</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+          </el-menu>
+        </div>
+        <div class="r">
+          <div class="search">
+
+          </div>
+        </div>
+      </div>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      alert(key, keyPath)
+    }
+  }
 }
 </script>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 const Hello = resolve => require(['@/views/Hello'], resolve)
 const NotFound = resolve => require(['@/views/NotFound'], resolve)
 const Signin = resolve => require(['@/views/Signin'], resolve)
+const index = resolve => require(['@/views/index'], resolve)
 const Routes = [
   {
     name: 'home',
@@ -11,6 +12,11 @@ const Routes = [
     name: 'signin',
     path: '/signin',
     component: Signin
+  },
+  {
+    name: 'index',
+    path: '/index',
+    component: index
   },
   {
     path: '*',
